01a4ccf61b078f396da890d542de1d42f50aa03d
Juan Sanin <jsanin@gmail.com>
2015-05-05 23:31:12 -0500

    Primera versión

---
+++ src/main/java/com/juansanin/spring/controller/configuration/MyWebappConfiguration.java
@@ -0,0 +1,13 @@
+package com.juansanin.spring.controller.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/*
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.juansanin.spring.controller")*/
+public class MyWebappConfiguration {
+
+}
